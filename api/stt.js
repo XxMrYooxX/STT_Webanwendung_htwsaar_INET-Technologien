570ef1c0acd9d96bdf3f150636d05e8277898535
Marcel Hesselbach <marcelhesselbach@live.de>
2022-01-15 16:58:12 +0100

    Added Angular Template
    Added empty Angular Project as NodeJS Module for frontend-development

--- api/stt.js
+++ api/stt.js
@@ -12,7 +12,19 @@ var model = new deepspeech.Model("../models/model_de.pb")
 
 
 appRouter.post('/stt', multer.single("sttfile"), (req, res) => {
-    console.log("[STT SERVICE] Service starting...");
-    
+    console.log("[STT SERVICE]: Service starting...");
+    let outputText = model.stt(req.file.buffer)
+    console.log(`[STT SERVICE]: STT Output: ${outputText}`);
+    if(outputText){
+        res.send({
+            success: true,
+            text: outputText
+        });
+    } else {
+        res.send({
+            success: false,
+            text: "SST failed, try again!"
+        });
+    }
 })
 module.exports = appRouter;
