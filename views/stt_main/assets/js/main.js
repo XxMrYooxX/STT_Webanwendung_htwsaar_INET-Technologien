4ad8406b085e58f44f5c92e41c8976b14f5391df
Marcel Hesselbach <marcelhesselbach@live.de>
2022-03-24 22:58:14 +0100

    Added Output Textbox + fixed JS response

--- views/stt_main/assets/js/main.js
+++ views/stt_main/assets/js/main.js
@@ -1,8 +1,18 @@
-document.getElementById('apply').addEventListener('change', function (event) {
+document.getElementById('apply').addEventListener('change', async function (event) {
     event.preventDefault();
     console.log("Du bist ein Hurensohn");
     const input = document.querySelector('input[type="file"]');
     const file = input.files[0];
+    const url = '/api/stt'
+    let data = new FormData()
+    data.append('sttfile', file)
+    const response = await fetch(url, {
+        method: 'POST',
+        body: data
+    })
+    let datajson = await response.json()
+    console.log(datajson)
+    document.getElementsByName('outputTextBox')[0].value = datajson.text
 }, false);
 
 function auswahl(event){
